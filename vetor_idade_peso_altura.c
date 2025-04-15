3416c4073e0ade05697311d44c6ed4ad3f9191d1
Enzo Henrique Favaro <enzo.h.favaro@gmail.com>
2025-04-15 19:20:15 -0300

    vetor_idade_peso_altura.c

--- vetor_idade_peso_altura.c
+++ vetor_idade_peso_altura.c
@@ -6,14 +6,14 @@ int idade[10],peso[10],altura[10];
 int i,PesoAltura=0, Idade10e30eAlt=0;
 float MediaIdade;
 
-for(i=0;i<10;i++){
-    printf("\nDigite sua idade: ");
-    scanf("%d", &idade[i]);
+    for(i=0;i<10;i++){
+        printf("\nDigite sua idade: ");
+        scanf("%d", &idade[i]);
 
-    printf("Digite seu peso em KG: ");
-    scanf("%d", &peso[i]);
-    printf("Digite sua altura em CM: ");
-    scanf("%d", &altura[i]);
+        printf("Digite seu peso em KG: ");
+        scanf("%d", &peso[i]);
+        printf("Digite sua altura em CM: ");
+        scanf("%d", &altura[i]);
         if(peso[i]>90 && altura[i]<150){
             PesoAltura++;
         }
@@ -21,6 +21,7 @@ for(i=0;i<10;i++){
             Idade10e30eAlt++;
         }
     }
+    
     MediaIdade = (idade[0]+idade[1]+idade[2]+idade[3]+idade[4]+idade[5]+idade[6]+idade[7]+idade[8]+idade[9])/10;
     printf("\nMedia das idades: %.2f", MediaIdade);
     printf("\n\nQuantidade de pessoas com mais de 90kg e menos de 1,50m: %i", PesoAltura);
